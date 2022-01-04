2533410baecafa5fcf38383fbba5834f21882261
DMDoom <77032549+DMDoom@users.noreply.github.com>
2022-01-04 22:30:55 +0100

    feat: add offer repository

---
+++ src/main/java/pl/dofizjo/dofizjoapplication/model/Offer.java
@@ -0,0 +1,21 @@
+package pl.dofizjo.dofizjoapplication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Offer {
+
+    private int id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+}
