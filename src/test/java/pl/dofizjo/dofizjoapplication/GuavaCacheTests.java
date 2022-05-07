a3b18316469368b0a57ba16836ce47d9fa3a1a6d
DMDoom <77032549+DMDoom@users.noreply.github.com>
2022-05-07 21:27:38 +0200

    refactor: clean and avoid code repetition

--- src/test/java/pl/dofizjo/dofizjoapplication/GuavaCacheTests.java
+++ src/test/java/pl/dofizjo/dofizjoapplication/GuavaCacheTests.java
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -15,23 +16,15 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest
 public class GuavaCacheTests {
 
-    private final int attemptLimit = 10;
-
     @Autowired
     LoginAttemptService loginAttemptService;
 
     @Test
     public void whenLoginAttemptLimitExceeded_thenIpBlocked() {
         // Given
+        int attemptLimit = loginAttemptService.getAttemptLimit();
         MockHttpServletRequest request = new MockHttpServletRequest();
-        String header = request.getHeader("X-Forwarded-For");
-        String ip;
-
-        if (header == null) {
-            ip = request.getRemoteAddr();
-        } else {
-            ip = header.split(",")[0];
-        }
+        String ip = getIp(request);
 
         // When
         for (int i = 0; i < attemptLimit; i++) {
@@ -45,15 +38,9 @@ public class GuavaCacheTests {
     @Test
     public void whenLoginAttemptSuccess_thenAttemptLimitReset() {
         // Given
+        int attemptLimit = loginAttemptService.getAttemptLimit();
         MockHttpServletRequest request = new MockHttpServletRequest();
-        String header = request.getHeader("X-Forwarded-For");
-        String ip;
-
-        if (header == null) {
-            ip = request.getRemoteAddr();
-        } else {
-            ip = header.split(",")[0];
-        }
+        String ip = getIp(request);
 
         // When
         for (int i = 0; i < attemptLimit / 2; i++) {
@@ -64,5 +51,15 @@ public class GuavaCacheTests {
 
         // Then
         assertThat(loginAttemptService.getAttemptsCache().size()).isEqualTo(0);
+    }
+
+    public String getIp(MockHttpServletRequest request) {
+        String header = request.getHeader("X-Forwarded-For");
+
+        if (header == null) {
+            return request.getRemoteAddr();
+        } else {
+            return header.split(",")[0];
+        }
     }
 }
