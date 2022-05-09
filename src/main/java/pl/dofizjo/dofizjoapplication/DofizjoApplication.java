305c9571dedbe50f47280b60a764d3aeb53f399b
DMDoom <77032549+DMDoom@users.noreply.github.com>
2022-05-09 19:30:01 +0200

    feat: add production account generation through env variables

--- src/main/java/pl/dofizjo/dofizjoapplication/DofizjoApplication.java
+++ src/main/java/pl/dofizjo/dofizjoapplication/DofizjoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,7 +14,11 @@ import pl.dofizjo.dofizjoapplication.model.*;
 
 @SpringBootApplication
 @Slf4j
+@ConfigurationProperties(prefix="dofizjo.security")
 public class DofizjoApplication {
+
+	String username;
+	String password;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DofizjoApplication.class, args);
@@ -24,9 +29,17 @@ public class DofizjoApplication {
 
 	@Bean
 	@Profile("dev")
-	CommandLineRunner dataLoader(PostRepository postRepo, PartnerRepository partnerRepo, ReviewRepository reviewRepo, BlockRepository blockRepo, MethodRepository methodRepo, UserRepository userRepo, CommentRepository commentRepo) {
+	CommandLineRunner dataLoaderDev(PostRepository postRepo, PartnerRepository partnerRepo, ReviewRepository reviewRepo, BlockRepository blockRepo, MethodRepository methodRepo, UserRepository userRepo, CommentRepository commentRepo) {
 		return args -> {
 			userRepo.add(new User("user", encoder.encode("password"), true));
+		};
+	}
+
+	@Bean
+	@Profile("prod")
+	CommandLineRunner dataLoaderProd(PostRepository postRepo, PartnerRepository partnerRepo, ReviewRepository reviewRepo, BlockRepository blockRepo, MethodRepository methodRepo, UserRepository userRepo, CommentRepository commentRepo) {
+		return args -> {
+			userRepo.add(new User(username, encoder.encode(password), true));
 		};
 	}
 }
